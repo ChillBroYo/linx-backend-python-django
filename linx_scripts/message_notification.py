4806040c11dbbc153c746a82c659e18fa5015830
Samson Tse <sammy_tse@msn.com>
2020-08-03 22:44:52 -0700

    Added running scripts on the prod machine

--- linx_scripts/message_notification.py
+++ linx_scripts/message_notification.py
@@ -66,25 +66,30 @@ def send_push_message(token, message, extra=None):
 
 sql_connect = sqlite3.connect('/home/ubuntu/linx-backend-python-django/linx/db.sqlite3')
 cursor = sql_connect.cursor()
-user_ids = []
+other_user_ids = []
 # Search for friends that are compatible
 query = "SELECT user_id, info from linx_luser ORDER BY user_id;"
 user_id_query = cursor.execute(query).fetchall()
 for row in user_id_query:
-    query = "SELECT COUNT(mid) FROM linx_messages WHERE user_id = {} AND (time_user_seen == NULL OR users_notified='false');".format(row[0])
+    query = "SELECT user_id, other_id FROM linx_messages WHERE user_id = {} AND (time_user_seen == NULL OR users_notified='0');".format(row[0])
     count_query = cursor.execute(query).fetchall()
-    if int(count_query[0][0]) > 0:
-        user_ids.append(row[0])
-        "ALTER TABLE linx_messages SET time_user_seen=\'{}\', users_notified=\'true\' WHERE user_id={}".format(datetime.datetime.now(), row[0])
-        count_query = cursor.execute(query)
+    if len(count_query) > 0:
+        for message_row in count_query:
+            if message_row[1] not in other_user_ids:
+                other_user_ids.append(message_row[1])
+            query2 = "UPDATE linx_messages SET time_user_seen=time(\'now\'), users_notified=\'true\' WHERE other_id={} AND user_id={};".format(message_row[1], message_row[0])
+        count_query = cursor.execute(query2)
+        sql_connect.commit()
 
+print("At {} sent notifications to users {}".format(datetime.datetime.now(), other_user_ids))
 sql_connect.close()
-
-for user_id in user_ids:
-    loaded_info = json.loads(user_id_query[user_id][1])
+for user_id in other_user_ids:
+    loaded_info = json.loads(user_id_query[int(user_id) - 1][1])
+    if loaded_info.get("expoPushToken") == None:
+        continue
     expo_push_token = loaded_info["expoPushToken"]
     data = {"user_id": "{}".format(user_id),
-         "timestamp": datetime.datetime.now(),
+         "timestamp": str(datetime.datetime.now()),
          "type": "message"
          }
     send_push_message(expo_push_token, "You have a new message", data) 
